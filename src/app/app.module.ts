443753ccbffc1c23637151751fd510b839eea65b
Svetlana Kim <svetlana.kim.7008@gmail.com>
2023-10-06 21:30:22 +0300

    add test tree component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+
+import { FormsModule } from '@angular/forms';
+import { FormComponentComponent } from './form-component/form-component.component';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule }   from '@angular/forms';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    FormComponentComponent,
+    AppComponent,
+    TestComponentComponent,
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
